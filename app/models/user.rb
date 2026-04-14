80e0c7405335c9932ba57132aea9b4484cf9889b
pigeon-shou <pigeonshow2024@gmail.com>
2026-04-14 20:12:37 +0900

    テストコード実装　バリデーション変更
    新たに正規表現を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,39 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items # dependent: :destroy 削除オプションつけるかはdestroy実装時検討
-  has_many :orders # dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_katakana, presence: true
-  validates :first_name_katakana, presence: true
   validates :birth_date, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+\z/
+  validates :password,
+            format: { with: VALID_PASSWORD_REGEX }, allow_blank: true,
+            if: :password_required?
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々ー]+\z/
+
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options format: { with: VALID_NAME_REGEX }, allow_blank: true do
+    validates :first_name
+    validates :last_name
+  end
+
+  VALID_KATAKANA_REGEX = /\A[ァ-ヶー]+\z/
+
+  with_options presence: true do
+    validates :first_name_katakana
+    validates :last_name_katakana
+  end
+
+  with_options format: { with: VALID_KATAKANA_REGEX }, allow_blank: true do
+    validates :first_name_katakana
+    validates :last_name_katakana
+  end
 end
