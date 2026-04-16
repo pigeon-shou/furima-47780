02617ac69262c0c6c4c544eb06501a400e12043e
pigeon-shou <pigeonshow2024@gmail.com>
2026-04-16 17:07:40 +0900

    DBへのデータ保存後一覧ページへ遷移
    軽いバリデーションをかけています

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,28 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  # ここまでActiveHashで紐付けた要素
+  belongs_to :user
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
+  validates :item_name, length: { maximum: 40 }
+  validates :description, length: { maximum: 1000 }
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  # 数値性のチェック+範囲指定
 end
